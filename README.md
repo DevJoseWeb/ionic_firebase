660b488b8677c5f2e97f00c5e3c30e1eb48af54f
Paula <web2ajax@gmail.com>
2018-06-19 02:50:32 -0300

    primeiro commit ionic firebase

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ionic +  Firebase + Google Cloud Vision
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { VisionPage } from '../pages/vision/vision';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+import { Camera } from '@ionic-native/camera';
+
+
+const firebaseConfig = {
+ // config
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    VisionPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    VisionPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Camera
+  ]
+})
+export class AppModule {}
